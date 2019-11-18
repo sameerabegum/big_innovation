253962344e16cb991fde9a3df697450a69fdff2d
Aflak Arshi <aflak.arshi@niyuj.com>
2019-11-19 00:16:54 +0530

    chat model added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BigInovation';
+
+  openChat() {
+    $('.ui.modal')
+    .modal('show');
+  }
 }
